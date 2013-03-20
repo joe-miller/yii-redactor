e588c58b844770fafc9475d86157b843a8310503
joe-miller <admin@northernculture.co.uk>
2013-03-20 18:22:16 +0000

    Update ImageList.php
    Added a function to find folders. Excludes folders '.', '..', 'tmp' and any hidden folders.
    Modified run method to find all t=folders for the drop-down list, and only those files that are requested by the url

--- actions/ImageList.php
+++ actions/ImageList.php
@@ -5,42 +5,73 @@
  *
  * @param string $attr Model attribute
  */
+class ImageList extends CAction {
 
-class ImageList extends CAction
-{
-	public $uploadPath;
-	public $uploadUrl;
+    public $uploadPath;
+    public $uploadUrl;
+    private $foldersArray = array();
 
-	public function run($attr)
-	{
-		$name=strtolower($this->getController()->getId());
-		$attribute=strtolower((string)$attr);
+    public function run($attr) {
+        $files = NULL;
+        $attr = '/' . $attr;
+        $name = strtolower($this->getController()->getId());
+        $attribute = strtolower((string) $attr);
 
-		if ($this->uploadPath===null) {
-			$path=Yii::app()->basePath.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'uploads';
-			$this->uploadPath=realpath($path);
-			if ($this->uploadPath===false) {
-				exit;
-			}
-		}
-		if ($this->uploadUrl===null) {
-			$this->uploadUrl=Yii::app()->request->baseUrl .'/uploads';
-		}
+        if ($this->uploadPath === null) {
+            $path = Yii::app()->basePath . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'uploads';
+            $this->uploadPath = realpath($path);
+            if ($this->uploadPath === false) {
+                exit;
+            }
+        }
+        if ($this->uploadUrl === null) {
+            $this->uploadUrl = Yii::app()->request->baseUrl . '/uploads';
+        }
 
-		$attributePath=$this->uploadPath.DIRECTORY_SEPARATOR.$name.DIRECTORY_SEPARATOR.$attribute;
-		$attributeUrl=$this->uploadUrl.'/'.$name.'/'.$attribute.'/';
+        $attributePath = str_replace('//', '/', $this->uploadPath.$attribute);
+        $attributeUrl = str_replace('//', '/', $this->uploadUrl.$attribute);
+        
+        if ($attr !== '/')
+        {
+            $files = CFileHelper::findFiles($attributePath, array('fileTypes' => array('gif', 'png', 'jpg', 'jpeg'), 'level'=>0));
+        }
+        
+        $folders = $this->getFolders($this->uploadPath);
+        foreach($folders as $folder)
+        {
+            if ($folder !=='.' && $folder !=='..')
+            $data[] = array(
+                'folder' => $folder,
+            );
+        }
+        
+        if ($files) {
+            foreach ($files as $file) {
+                $data[] = array(
+                'thumb' => str_replace('//', '', $attributeUrl . '/' . pathinfo($file, PATHINFO_BASENAME)),
+                'image' => str_replace('//', '', $attributeUrl . '/' . pathinfo($file, PATHINFO_BASENAME)),
+                );
+            }
+        }
+        echo CJSON::encode($data);
+        exit;
+    }
+    
+    private function getFolders($path)
+    {
+        $folders = scandir($path);
+        foreach ($folders as $folder)
+        {
+            if($folder !== '.' && $folder !=='..' && $folder[0] !== '.' && $folder !== 'tmp')
+            {
+                if (is_dir($path . '/' . $folder))
+                {
+                    $this->foldersArray[] = substr(str_replace(realpath(Yii::app()->basePath . '/../images/'), '', $path) . '/' . $folder, 1);
+                    $this->getFolders($path . '/' . $folder);
+                }
+            }
+        }
+        return $this->foldersArray;
+    }
 
-		$files=CFileHelper::findFiles($attributePath,array('fileTypes'=>array('gif','png','jpg','jpeg')));
-		$data=array();
-		if ($files) {
-			foreach($files as $file) {
-				$data[]=array(
-					'thumb'=>$attributeUrl.basename($file),
-					'image'=>$attributeUrl.basename($file),
-				);
-			}
-		}
-		echo CJSON::encode($data);
-		exit;
-	}
 }
